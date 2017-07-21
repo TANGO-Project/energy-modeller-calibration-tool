a8f838bb7ab9454ae1e2254c2ec086dea2233386
Richard Kavanagh <r_kav_0000@hotmail.com>
2017-07-21 10:40:28 +0100

    Ensuring scripts don't append to files.
    The aim of this is to avoid new header's being written every time the
    script is restarted, as these headers will be located midway through the
    file, which isn't wanted.

--- Nvidia GPU Calibration Scripts/gather_calibration_data.sh
+++ Nvidia GPU Calibration Scripts/gather_calibration_data.sh
@@ -6,6 +6,6 @@ let GPU_COUNT-=1
 #Note: using the parameter -f ./GPU-stats.csv forces the script to run in the foreground. Redirecting standard out i.e. >> ./GPU-stats.csv does not have this limitation.
 for ((GPU=0;GPU <= $GPU_COUNT; GPU++)); do
    NAME=`nvidia-smi --query-gpu=name --format=csv,nounits,noheader -i $GPU`
-   nvidia-smi --query-gpu=utilization.gpu,utilization.memory,temperature.gpu,clocks.current.graphics,clocks.current.sm,clocks.current.memory,clocks.current.video,power.draw --format=csv,nounits  -i $GPU -l 1 >> ./$HOSTNAME-GPU-"$NAME"-"$GPU"-stats.csv &
+   nvidia-smi --query-gpu=utilization.gpu,utilization.memory,temperature.gpu,clocks.current.graphics,clocks.current.sm,clocks.current.memory,clocks.current.video,power.draw --format=csv,nounits  -i $GPU -l 1 > ./$HOSTNAME-GPU-"$NAME"-"$GPU"-stats.csv &
    echo "$!" > $GPU.pid
 done
