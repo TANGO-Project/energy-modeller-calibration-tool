9cf76bc3657594943857a5de34d0da1676f72ec0
Richard Kavanagh <r_kav_0000@hotmail.com>
2017-07-24 09:39:11 +0100

    Correcting stop script

--- Nvidia GPU Calibration Scripts/stop_gathering.sh
+++ Nvidia GPU Calibration Scripts/stop_gathering.sh
@@ -1,7 +1,9 @@
-OUNT=`nvidia-smi --query-gpu=gpu_name --format=csv,noheader | wc -l`
+#!/bin/bash
+
+GPU_COUNT=`nvidia-smi --query-gpu=gpu_name --format=csv,noheader | wc -l`
 let GPU_COUNT-=1
 
 #Note: using the parameter -f ./GPU-stats.csv forces the script to run in the foreground. Redirecting standard out i.e. >> ./GPU-stats.csv does not have this limitation.
 for ((GPU=0;GPU <= $GPU_COUNT; GPU++)); do
-   kill $(cat "$GPU".pid)
+   kill -9 $(cat "$GPU".pid)
 done
